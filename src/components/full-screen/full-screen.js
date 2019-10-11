9bd789c4af0d39a8cd67fcc73e1aecf2330ec7ed
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-10-11 16:20:53 +0700

    Сделал кнопку Вернуться назад

--- src/components/full-screen/full-screen.js
+++ src/components/full-screen/full-screen.js
@@ -19,30 +19,31 @@ function FullScreen(props) {
     result = data.find((item, i) => i == id);
     localStorage.setItem('lastPicture', JSON.stringify(result));
   }
-  console.log(result);
   
   let d = result.created_at;
   d = d.split('T')[0].split('-').reverse().join(".");
-  console.log(d);
 
   let toggleLike = "Поставить" || "Убрать";
 
   return (
-    <div className="foto-wrap-fs">
-      <img src={result.urls.full} alt={"Фотография " + result.user.name} className="foto-fs"/>
-      <div className="cart-image-fs">
-        <div className="cart-image__autor-fs">
-          <img src={result.user.profile_image.medium} alt={"Аватарка " + result.user.name} className="foto-autor-fs"/>
-          <a href={result.user.links.html} target="_blank" className="name-autor-fs">{result.user.name}</a>
-        </div>
-        <time className="date-foto-fs" itemProp="datePublished" dateTime={result.created_at}>{d}</time>
-        <div className="cart-image__data-fs">
-          <button className="toggle-like-fs" aria-label="Поставить лайк или дизлайк">{toggleLike} LIKE</button>
-          <img src="img/likes.png" alt="Количество лайков" className="foto-likes-fs"/>
-          <span className="number-likes-fs">{result.likes}</span>
+    <React.Fragment>
+      <button className="come-back-fs" onClick={() => window.history.back()}>&#8592; Вернуться назад</button>
+      <div className="foto-wrap-fs">
+        <img src={result.urls.full} alt={"Фотография " + result.user.name} className="foto-fs"/>
+        <div className="cart-image-fs">
+          <div className="cart-image__autor-fs">
+            <img src={result.user.profile_image.medium} alt={"Аватарка " + result.user.name} className="foto-autor-fs"/>
+            <a href={result.user.links.html} target="_blank" className="name-autor-fs">{result.user.name}</a>
+          </div>
+          <time className="date-foto-fs" itemProp="datePublished" dateTime={result.created_at}>{d}</time>
+          <div className="cart-image__data-fs">
+            <button className="toggle-like-fs">{toggleLike} LIKE</button>
+            <img src="img/likes.png" alt="Количество лайков" className="foto-likes-fs"/>
+            <span className="number-likes-fs">{result.likes}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
 
